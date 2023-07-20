a9dde503dfcabb0cc1380555a0052bf411f6d318
yuwei <yu-wei-1987@163.com>
2023-07-20 16:19:16 +0800

    [IMP]:test-增加注释

--- src/main/java/com/uiero/StudySpringbootApplication.java
+++ src/main/java/com/uiero/StudySpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author yuwei
+ * @author yuwei 增加测试分支注释
  */
 @SpringBootApplication
 @MapperScan("com.uiero.spring.mapper")
