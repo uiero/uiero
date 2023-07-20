7e448ee333fb2e99d848fd629a9595e5b9756d22
yuwei <yu-wei-1987@163.com>
2023-07-20 16:05:50 +0800

    [IMP]:分支测试

--- src/main/java/com/uiero/StudySpringbootApplication.java
+++ src/main/java/com/uiero/StudySpringbootApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudySpringbootApplication {
 
     public static void main(String[] args) {
+        System.out.println("创建dev分支");
         SpringApplication.run(StudySpringbootApplication.class, args);
     }
 
